41763dc5d79f5674a9c0adf2904051b9b33e63e7
Sereja Khazov <sergeykhazov1@gmail.com>
2021-11-16 18:38:34 +0300

    module 21

---
+++ module21/homework212011/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  homework212011
+//
+//  Created by Сережа Хазов on 14.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstCircle: CircleView!
+    @IBOutlet weak var secondCircle: CircleView!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func panActionOne(_ gesture: UIPanGestureRecognizer) {
+        
+        let firstCircleFrame = firstCircle.frame
+        let secondCircleFrame = secondCircle.frame
+        
+        for value in Int(secondCircleFrame.minY)...Int(secondCircleFrame.maxY) {
+            if Int(firstCircleFrame.origin.y) == value {
+                secondCircle.isHidden = true
+                self.firstCircle.backgroundColor = .blue
+                self.firstCircle.transform.a = 1.5
+                self.firstCircle.transform.d = 1.5
+
+            }
+        }
+        
+        
+        let gestureTranslation = gesture.translation(in: view)
+        guard let gestureView = gesture.view else { return }
+        gestureView.center = CGPoint (x: gestureView.center.x + gestureTranslation.x, y: gestureView.center.y + gestureTranslation.y)
+        
+        gesture.setTranslation(.zero, in: view)
+        
+        guard gesture.state == .ended else { return }
+    }
+    
+}
+
